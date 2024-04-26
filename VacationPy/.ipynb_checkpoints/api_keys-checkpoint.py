c256ff2da40c13a22af76cf601bb5b51945ea8e4
Katrina <kat_witt@bigpond.com>
2024-04-26 13:17:08 +1000

    Corrected order of lgn and lat arguments in VacationPy.ipynb

--- VacationPy/.ipynb_checkpoints/api_keys-checkpoint.py
+++ VacationPy/.ipynb_checkpoints/api_keys-checkpoint.py
@@ -1,1 +1,5 @@
-geoapify_key = "YOUR API KEY HERE"
+# OpenWeatherMap API Key
+weather_api_key = "e49814dc8f19ce39f707cbfc36afeec0"
+
+# Geoapify API Key
+geoapify_key = "35b1fbb4e7a84cf6b398cad6764fab6f"
