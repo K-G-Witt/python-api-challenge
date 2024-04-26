872bb2ff4c12c92a3bc61d3a3046911831ba63e6
K-G-Witt <156146173+K-G-Witt@users.noreply.github.com>
2024-04-26 13:21:00 +1000

    Update api_keys-checkpoint.py

--- VacationPy/.ipynb_checkpoints/api_keys-checkpoint.py
+++ VacationPy/.ipynb_checkpoints/api_keys-checkpoint.py
@@ -1,5 +1,2 @@
-# OpenWeatherMap API Key
-weather_api_key = "e49814dc8f19ce39f707cbfc36afeec0"
-
 # Geoapify API Key
-geoapify_key = "35b1fbb4e7a84cf6b398cad6764fab6f"
+geoapify_key = "YOUR API HERE"
